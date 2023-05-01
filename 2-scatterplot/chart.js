987d2522a037cc2767ad0bd16a283823798954d3
Reza Sobhgol <rezatm331@gmail.com>
2023-05-02 01:07:30 +0330

    (chart:scatterplot) : add axises

--- 2-scatterplot/chart.js
+++ 2-scatterplot/chart.js
@@ -1,6 +1,9 @@
 // import d3 from "d3";
+
 const xAccessor = (d) => d.dewPoint;
 const yAccessor = (d) => d.humidity;
+
+const colorAccessor = (d) => d.cloudCover;
 
 const lowestDimensions = d3.min([window.innerHeight * 0.9, window.innerWidth * 0.9]);
 const dimensions = {
@@ -24,11 +27,13 @@ const drawChart = async () => {
     .select("#wrapper")
     .append("svg")
     .attr("width", dimensions.width)
-    .attr("height", dimensions.height);
+    .attr("height", dimensions.height)
+    .style("background", "#eeeeee");
 
   const bounds = wrapper
     .append("g")
     .style("transform", `translate(${dimensions.margin.left}px , ${dimensions.margin.top}px)`);
+  console.log(bounds);
 
   const xScale = d3
     .scaleLinear()
@@ -42,15 +47,47 @@ const drawChart = async () => {
     .range([dimensions.boundsHeight, 0])
     .nice();
 
+  const colorScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataSet, colorAccessor))
+    .range(["skyblue", "darkslategrey"]);
+
   const dots = bounds
     .selectAll("circle")
     .data(dataSet)
-    .enter()
-    .append("circle")
+    .join("circle")
     .attr("cx", (d) => xScale(xAccessor(d)))
     .attr("cy", (d) => yScale(yAccessor(d)))
     .attr("r", 3)
-    .attr("fill", "gray");
+    .attr("fill", (d) => colorScale(colorAccessor(d)));
+
+  // xAxis line
+  const xAxisGenerator = d3.axisBottom().scale(xScale).ticks(20);
+  const xAxis = bounds.append("g");
+  xAxis.call(xAxisGenerator).style("transform", `translateY(${dimensions.boundsHeight - 10}px)`);
+
+  const xAxisLabel = xAxis
+    .append("text")
+    .attr("x", dimensions.boundsWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .html("Dew point (&deg;F)");
+
+  // yAxis line
+  const yAxisGenerator = d3.axisLeft().scale(xScale).ticks(4);
+  const yAxis = bounds.append("g");
+  yAxis.call(yAxisGenerator);
+
+  const yAxisLabel = yAxis
+    .append("text")
+    .attr("x", -dimensions.boundsHeight / 2)
+    .attr("y", -dimensions.margin.left + 20)
+    .attr("fill", "black")
+    .style("transform", "rotate(-90deg)")
+    .style("font-size", "1.4em")
+    .style("text-anchor", "middle")
+    .html("Relative humidity");
 };
 
 await drawChart();
