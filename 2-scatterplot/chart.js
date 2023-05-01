d43e30ec7dc4c56192920e2e14c9bec1c7c83842
Reza Sobhgol <rezatm331@gmail.com>
2023-05-01 23:17:26 +0330

    (chart:scatterplot) : add xScale

---
+++ 2-scatterplot/chart.js
@@ -0,0 +1,40 @@
+import * as d3 from "d3";
+
+const xAccessor = (d) => d.dewPoint;
+const yAccessor = (d) => d.humidity;
+
+const lowestDimensions = d3.min([window.innerHeight * 0.9, window.innerWidth * 0.9]);
+const dimensions = {
+  width: lowestDimensions,
+  height: lowestDimensions,
+  margin: {
+    top: 10,
+    right: 10,
+    left: 60,
+    bottom: 50,
+  },
+};
+
+dimensions.boundsWidth = dimensions.width - dimensions.margin.right - dimensions.margin.left;
+dimensions.boundsHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+const drawChart = async () => {
+  const dataSet = await d3.json("./my_weather_data.json");
+
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style("transform", `translate(${dimensions.margin.left}px , ${dimensions.margin.top}px)`);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataSet, xAccessor))
+    .range([0, dimensions.boundsWidth]);
+};
+
+await drawChart();
